29fb3f4c3f44565e026d8bdc31a14e3c83467372
Vipning <2799558010@qq.com>
2020-07-31 09:24:41 +0800

    Update run_bert.py

--- bert-crf-mn/run_bert.py
+++ bert-crf-mn/run_bert.py
@@ -163,9 +163,9 @@ def predict(args,processor):
             aux_sentence = [train_data[i] for i in test_train[step][:args.aug_num]]
             for s in aux_sentence:
                 a_input_ids.append(s['input_ids'])
-                a_label_ids.append(s['label_ids'])
+#                 a_label_ids.append(s['label_ids'])
                 #地址信息增强，将所有的标签信息改成adress标签，全1
-                #a_label_ids.append(s['input_mask'])
+                a_label_ids.append(s['input_mask'])
                 a_input_mask.append(s['input_mask'])
                 a_input_lens.append(s['input_len'])
                 a_segment_ids.append(s['segment_ids'])
